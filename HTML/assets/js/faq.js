a84b8ab53794e4f0566b90fa1cf4ade80de599e6
Shailesh-294 <slakhani062@rku.ac.in>
2021-12-04 18:11:51 +0530

    added tab button in faq page

--- HTML/assets/js/faq.js
+++ HTML/assets/js/faq.js
@@ -1,22 +1,28 @@
 $(document).ready(function () {
-    $(".Rectangle-21-copy-3").click(function () {
+    $(".customer-tab").click(function () {
         $(".service-provider").css("display", "none");
         $(".customer").css("display", "block");
-        $(".Rectangle-21-copy-3").addClass("active");
-        $(".Rectangle-21").removeClass("active");
+        $(".customer-tab").addClass("active");
+        $(".servicer-tab").removeClass("active");
     });
-    $(".Rectangle-21").click(function () {
+    $(".servicer-tab").click(function () {
         $(".customer").css("display", "none");
         $(".service-provider").css("display", "block");
-        $(".Rectangle-21").addClass("active");
-        $(".Rectangle-21-copy-3").removeClass("active");
+        $(".servicer-tab").addClass("active");
+        $(".customer-tab").removeClass("active");
     });
 
-    $('.qe').on('click', function () {
-        if ($(this).hasClass("collapsed")) {
-            $(this).children('img').attr('src', "./assets/images/faq/arrow-right.png");
+    $('.card1 .qe').on('click', function () {
+        //$(this).css("background-color","black");
+        //alert($(this).hasClass("collapsed"));
+        var clist = document.getElementsByClassName("card1");
+        for(var i=0;i<clist.length;i++){
+           clist[i].getElementsByClassName("img")[0].setAttribute('src', "./assets/images/faq/arrow-right.png");
+        }
+        if (!$(this).hasClass("collapsed")) {
+            $(this).find('.img').attr('src', "./assets/images/faq/arrow-right.png");
         } else {
-            $(this).children('img').attr('src', "./assets/images/faq/arrow-down.png");
+            $(this).find('.img').attr('src', "./assets/images/faq/arrow-down.png");
         }
     });
 });

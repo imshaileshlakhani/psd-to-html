257f820329db8c04d4a690fb815bfca4e7eea081
Shailesh-294 <slakhani062@rku.ac.in>
2022-01-21 21:19:25 +0530

    book now page added

---
+++ HTML/assets/js/book-now.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    // extra-service from book now page
+    $(".extra-service label").click(function(){
+        $imgVal = $(this).find('input[type=checkbox]').val();
+        if ($(this).find('input[type=checkbox]').is(':checked')) {
+            $(this).find('div').addClass('active');
+            $(this).find('div img').attr("src","assets/images/book-now/"+$imgVal+"-green.png");
+        } else {
+            $(this).find('div').removeClass('active');
+            $(this).find('div img').attr("src","assets/images/book-now/"+$imgVal+".png");
+        }
+    });
+  
+    //card number validation
+    $("#cnumber").on("keyup", function (event) {
+      var value = $(this).val();
+      if (value.length <= 17) {
+        if (event.which >= 48 && event.which <= 57) {
+          var temp = value.replace(/\s/g, "");
+          var len = temp.length;
+          if (len % 4 == 0) {
+            $(this).val(value + " ");
+            flag = 1;
+          }
+        } else {
+          $(this).val(value.substring(0, value.length - 1));
+        }
+      } else {
+        $(this).val(value.substring(0, 18));
+        return false;
+      }
+    });
+  
+    //card date validation
+    $("#cmonth").on("keyup", function (event) {
+      var date = $(this).val();
+      if (date.length <= 5) {
+        if (event.which >= 48 && event.which <= 57) {
+          if (date.length == 2) {
+            $(this).val(date + "/");
+          }
+        } else {
+          $(this).val(date.substring(0, date.length - 1));
+        }
+      } else {
+        $(this).val(date.substring(0, 5));
+        return false;
+      }
+    });
+  
+    //card cvc validation
+    $("#ccvc").on("keyup", function(event){
+      var cvc = $(this).val();
+      if (event.which >= 48 && event.which <= 57) {
+          if (!cvc.length <= 3) {
+              $(this).val(cvc.substring(0, 3));    
+          }
+        } else {
+          $(this).val(cvc.substring(0, cvc.length - 1));
+        }
+    });
+  });
+  
--- HTML/assets/js/nav.js
+++ HTML/assets/js/nav.js
@@ -18,5 +18,6 @@ $(document).ready(function(){
     jQuery('.dropdown-menu li a').click(function(){
         var _this_img = jQuery(this).attr('data-img');
         jQuery(this).closest('.btn-group').find(' .dropdown-toggle img').attr('src',_this_img);
-      })
+    });
+	  
 });

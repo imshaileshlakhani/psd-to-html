7846afe37f23ca2e91122782697aa976c4ee44a8
Shailesh Lakhani <64061039+imshaileshlakhani@users.noreply.github.com>
2022-01-28 18:41:32 +0530

    Update config.php

--- Helperland/Controller/config.php
+++ Helperland/Controller/config.php
@@ -8,13 +8,14 @@ class Config{
     const DB_NAME = "helperland";
     
     /*------------ Users Constant--------------*/
-    const SESSION_DESTROY_TIME = 60*60*1; // 1 hour (60sec * 60min * 1hour)
 
     // 1 = customer, 2 = servicer, 3 = admin
     const USER_TYPE = [1,2,3];
 
     const SMTP_HOST = "smtp.gmail.com";
     const SMTP_EMAIL = "slakhani062@rku.ac.in";
-    const SMTP_PASS = "LSNH294@9216";
+    
+    //Enter password of SMTP_EMAIL to activate email functionality
+    const SMTP_PASS = "";
 }
 ?>
